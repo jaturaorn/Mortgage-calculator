ba6d6b44d4e830f3f63238a2275b268fac5db3dc
jaturaporn <83279262+jaturaorn@users.noreply.github.com>
2024-04-25 11:15:59 +0700

    make note code what the these is in Navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -5,9 +5,16 @@ import { Container } from "@mui/system";
 
 const Navbar = () => {
   return (
+    // *TODO The Appbar component of Material UI is used for creating a top navigation bar in the user interface.
     <AppBar position="static">
+      {/* Container component of Material UI is used for creating a container element 
+      that can be used to create a responsive layout, and center and contain other elements in a user interface.  */}
       <Container maxWidth="xl">
+        {/* Toolbar component can contain elements such as buttons, text, and icons, 
+        and can also be used for creating a responsive layout that adapts to different screen sizes. */}
         <Toolbar>
+          {/* typography component is used for applying pre-defined typographic styles to text elements. 
+          It helps create a consistent and visually pleasing layout with customizable font family, size, weight and spacing. */}
           <Typography variant="h5">Bank of React</Typography>
         </Toolbar>
       </Container>
