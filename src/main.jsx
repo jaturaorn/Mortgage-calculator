1a9147145ffc2dfca65af62742f9f6868e5727d6
jaturaporn <83279262+jaturaorn@users.noreply.github.com>
2024-04-25 11:04:51 +0700

    first commit & finish

---
+++ src/main.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+
+import { ThemeProvider } from "@mui/material";
+import CssBaseline from "@mui/material/CssBaseline";
+import { theme } from "./theme.js";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <ThemeProvider theme={theme}>
+      <App />
+      <CssBaseline />
+    </ThemeProvider>
+  </React.StrictMode>
+);
